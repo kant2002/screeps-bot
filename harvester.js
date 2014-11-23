99557dcd70dce9ab335e40c237cf0ea54da840d2
Andrey Kurdyumov <kant2002@gmail.com>
2014-11-23 16:48:06 +0600

    Fix harvesting

--- harvester.js
+++ harvester.js
@@ -1,15 +1,14 @@
 /* global Game, console, module */
 module.exports = function (creep) {
 	if(creep.energy < creep.energyCapacity) {
-        var sources = creep.pos.findNearest(Game.SOURCES_ACTIVE, { filter: function(object) { return object.energy >= 10; }});
-		if (sources.length === 0) {
+        var sources = creep.pos.findNearest(Game.SOURCES, { filter: function(object) { return object.energy >= 10; }});
+		if (sources === null) {
 		    console.log("No active sources");
 		} else {
     		creep.moveTo(sources);
     		creep.harvest(sources);
 		}
-	}
-	else {
+	} else {
 		creep.moveTo(Game.spawns.Spawn1);
 		creep.transferEnergy(Game.spawns.Spawn1);
 	}
