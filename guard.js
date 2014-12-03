013ed54206e0832dad48094f3bfb982afdd42116
Andrey Kurdyumov <kant2002@gmail.com>
2014-12-03 13:01:33 +0600

    Comeback when enemies far away

--- guard.js
+++ guard.js
@@ -1,14 +1,32 @@
 /* global Game, console, module */
+function distance(pos1, pos2) {
+    var dx = pos1.x - pos2.x;
+    var dy = pos1.y - pos2.y;
+    return Math.sqrt(dx * dx + dy * dy);
+}
+
+function cdistance(pos1, pos2) {
+    var dx = pos1.x - pos2.x;
+    var dy = pos1.y - pos2.y;
+    return Math.max(dx, dy);
+}
+
 module.exports = function (creep, targets, defendedSpawn) {
+    var spawnDistance = distance(creep.pos, defendedSpawn.pos);
     if (targets.length !== 0) {
         var targetToAttack = creep.pos.findNearest(Game.HOSTILE_CREEPS, { maxOps: 500 });
-        if (creep.pos.inRangeTo(defendedSpawn, 2) || creep.pos.inRangeTo(targetToAttack, 3)) {
+        var targetDistance = targetToAttack === null ? 100000 : distance(creep.pos, targetToAttack.pos);
+        if (spawnDistance <= 2 || targetDistance <= 3) {
             creep.moveTo(targetToAttack);
+        } else {
+            if (spawnDistance > 4) {
+                creep.moveTo(defendedSpawn);
+            }
         }
 
         creep.attack(targetToAttack);
     } else {
-        if (!creep.pos.inRangeTo(defendedSpawn, 3)) {
+        if (spawnDistance > 4) {
             creep.moveTo(defendedSpawn);
         }
     }
